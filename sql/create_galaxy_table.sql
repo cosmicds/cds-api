64fb6bccceed5f8ba6979f3703ac3697ee8719e1
Jon Carifio <carifio24@gmail.com>
2022-04-29 12:16:11 -0400

    Update galaxy table SQL definition and Galaxy model.

--- sql/create_galaxy_table.sql
+++ sql/create_galaxy_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE Galaxies(
     type varchar(10) NOT NULL,
     element varchar(10) NOT NULL DEFAULT "H-α",
     marked_bad tinyint(2) NOT NULL DEFAULT 0,
+    is_bad tinyint(2) NOT NULL DEFAULT 0,
 
     PRIMARY KEY(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
