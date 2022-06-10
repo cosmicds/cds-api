7582c999b602e5f63615e9a15d9295c2c0cbd75a
Jon Carifio <carifio24@gmail.com>
2022-06-10 18:39:37 -0400

    Add SQL definitions for sync and async class merge tables.

---
+++ sql/create_async_students_merged_classes.sql
@@ -0,0 +1,27 @@
+CREATE TABLE AsyncMergedHubbleStudentClasses (
+
+    /*
+    The student ID.
+    The student is in the class specified by class_id
+    */
+    student_id int(11) UNSIGNED NOT NULL UNIQUE,
+
+    /*
+    The ID of the class that needs more data
+    i.e. it's too small
+    */
+    class_id int(11) UNSIGNED NOT NULL,
+
+    /* The ID of the class that's merged in */
+    merged_class_id int(11) UNSIGNED NOT NULL,
+
+    merged datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY(student_id, class_id, merged_class_id),
+    INDEX(student_id),
+    ADD CONSTRAINT FK_Student_Class
+    FOREIGN KEY(student_id, class_id)
+    REFERENCES StudentsClasses(student_id, class_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
---
+++ sql/create_sync_merged_classes.sql
@@ -0,0 +1,23 @@
+CREATE TABLE SyncMergedHubbleClasses (
+
+    /*
+    The ID of the class that needs more data
+    i.e. it's too small
+    */
+    class_id int(11) UNSIGNED NOT NULL UNIQUE,
+
+    /* The ID of the class that's merged in */
+    merged_class_id int(11) UNSIGNED NOT NULL,
+
+    merged datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (class_id, merged_class_id),
+    FOREIGN KEY (class_id)
+        REFERENCES Classes(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (merged_class_id)
+        REFERENCES Classes(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
