ff174b730270dda6ac924963b4e493347b5347dd
Carifio24 <carifio24@gmail.com>
2022-11-09 11:49:38 -0500

    Add sample measurement model file.

---
+++ src/stories/hubbles_law/models/sample_measurement.ts
@@ -0,0 +1,81 @@
+import { Galaxy } from "./galaxy";
+import { Student } from "../../../models";
+import { Sequelize, DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "sequelize";
+
+export class SampleHubbleMeasurement extends Model<InferAttributes<SampleHubbleMeasurement>, InferCreationAttributes<SampleHubbleMeasurement>> {
+  declare student_id: number;
+  declare galaxy_id: number;
+
+  declare rest_wave_value: CreationOptional<number | null>;
+  declare rest_wave_unit: CreationOptional<string | null>;
+  declare obs_wave_value: CreationOptional<number | null>;
+  declare obs_wave_unit: CreationOptional<string | null>;
+  declare velocity_value: CreationOptional<number | null>;
+  declare velocity_unit: CreationOptional<string | null>;
+  declare ang_size_value: CreationOptional<number | null>;
+  declare ang_size_unit: CreationOptional<string | null>;
+  declare est_dist_value: CreationOptional<number | null>;
+  declare est_dist_unit: CreationOptional<string | null>;
+  declare last_modified: CreationOptional<Date>;
+}
+
+export function initializeSampleHubbleMeasurementModel(sequelize: Sequelize) {
+  SampleHubbleMeasurement.init({
+    student_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: Student,
+        key: "id"
+      },
+    },
+    galaxy_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: Galaxy,
+        key: "id"
+      }
+    },
+    rest_wave_value: {
+      type: DataTypes.FLOAT
+    },
+    rest_wave_unit: {
+      type: DataTypes.STRING
+    },
+    obs_wave_value: {
+      type: DataTypes.FLOAT
+    },
+    obs_wave_unit: {
+      type: DataTypes.STRING
+    },
+    velocity_value: {
+      type: DataTypes.FLOAT
+    },
+    velocity_unit: {
+      type: DataTypes.STRING
+    },
+    ang_size_value: {
+      type: DataTypes.FLOAT
+    },
+    ang_size_unit: {
+      type: DataTypes.STRING
+    },
+    est_dist_value: {
+      type: DataTypes.FLOAT
+    },
+    est_dist_unit: {
+      type: DataTypes.STRING
+    },
+    last_modified: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+    }
+  }, {
+    sequelize,
+    engine: "InnoDB"
+  });
+}
