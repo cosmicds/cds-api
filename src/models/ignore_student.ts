e94bc3e44424e2f635d5566125c27feca5919b86
Carifio24 <carifio24@gmail.com>
2023-05-18 17:11:10 -0400

    Add IgnoreStudents SQL and model.

---
+++ src/models/ignore_student.ts
@@ -0,0 +1,34 @@
+import { Sequelize, DataTypes, Model, InferAttributes, InferCreationAttributes } from "sequelize";
+import { Story } from "./story";
+import { Student } from "./student";
+
+export class IgnoreStudent extends Model<InferAttributes<IgnoreStudent>, InferCreationAttributes<IgnoreStudent>> {
+  declare student_id: number;
+  declare story_name: string;
+}
+
+export function initializeIgnoreStudentModel(sequelize: Sequelize) {
+  IgnoreStudent.init({
+    student_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: Student,
+        key: "id"
+      }
+    },
+    story_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: Story,
+        key: "name"
+      }
+    }
+  }, {
+    sequelize,
+    engine: "InnoDB"
+  });
+}
