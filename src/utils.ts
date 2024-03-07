07f3127cdaf2bfed3dc7a351c3265145c042cafa
Carifio24 <carifio24@gmail.com>
2024-03-07 01:47:29 -0500

    Add utility type for describing model updates.

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,11 @@ import { nanoid } from "nanoid";
 import { enc, SHA256 } from "crypto-js";
 import { v5 } from "uuid";
 
+import { Model } from "sequelize";
+
+// This type describes objects that we're allowed to pass to a model's `update` method
+export type UpdateAttributes<M extends Model> = Parameters<M["update"]>[0];
+
 export function createVerificationCode(): string {
   return nanoid(21);
 }
