0b3bcb3370c281e4e131e7a25c45110ad74cb046
Carifio24 <carifio24@gmail.com>
2024-09-15 02:15:15 -0400

    Add Jest config file.

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+  ]
+};
+
+export default config;
