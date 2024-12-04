3f08ff8da157b03aab279972d6c9b60cb87d466a
Carifio24 <carifio24@gmail.com>
2024-12-04 01:19:41 -0500

    Add registry for handling class setup functions.

---
+++ src/registries.ts
@@ -0,0 +1,34 @@
+import { Class } from "./models";
+
+type ClassSetupFunction = (cls: Class, storyName: string) => void;
+
+class ClassSetupRegistry {
+
+  private members: { [storyName: string]: ClassSetupFunction[] | undefined } = {};
+  
+  register(setup: ClassSetupFunction, storyName: string) {
+    if (!(storyName in this.members)) {
+      this.members[storyName] = [];
+    }
+    this.members[storyName]?.push(setup);
+  }
+
+  unregister(setup: ClassSetupFunction, storyName: string) {
+    const setups = this.members[storyName];
+    if (!setups) {
+      return;
+    }
+
+    const index = setups.indexOf(setup);
+    if (index > -1) {
+      setups.splice(index, 1);
+    }
+  }
+
+  setupFunctions(storyName: string): ClassSetupFunction[] | undefined {
+    return this.members[storyName];
+  }
+
+}
+
+export const classSetupRegistry = new ClassSetupRegistry();
