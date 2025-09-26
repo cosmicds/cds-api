9005fd17ab97a02620a0c4576525595108092da6
Carifio24 <carifio24@gmail.com>
2025-09-26 12:03:33 -0400

    Add (trivial) route for Storybook 'user experience' submission.

--- src/stories/storybook/router.ts
+++ src/stories/storybook/router.ts
@@ -1,3 +1,10 @@
-import { Router } from "express";
+import { Express, Router } from "express";
+import { Sequelize } from "sequelize";
 
 export const router = Router();
+
+export function setup(_app: Express, _db: Sequelize) {}
+
+router.put("/user-experience", async (_req, res) => {
+  res.status(200).json({ success: true });
+});
