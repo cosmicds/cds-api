db581e0d5652ccbacf2c8f62aff43cc54b42d876
Carifio24 <carifio24@gmail.com>
2025-10-08 13:59:43 -0400

    Don't run middleware check on permissions endpoint.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import { ALLOWED_ORIGINS } from "./utils";
 
 export async function apiKeyMiddleware(req: Request, res: ExpressResponse, next: NextFunction): Promise<void> {
 
-  if (req.originalUrl === "/") {
+  if (["/", "/permission"].includes(req.path)) {
     next();
     return;
   }
