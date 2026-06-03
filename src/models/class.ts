732295911c77ddc3ac5750bf15ac76896215e24d
Carifio24 <carifio24@gmail.com>
2026-06-03 01:34:00 -0400

    Add model for active class. Refactor code in class model to facilitate not duplicating code.

--- src/models/class.ts
+++ src/models/class.ts
@@ -1,7 +1,7 @@
 import { Educator } from "./educator";
 import { Sequelize, DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "sequelize";
 
-const CLASS_STATUS_VALUES = ["seed", "real_good_data", "real_bad_data", "test_good_data", "test_bad_data"] as const;
+export const CLASS_STATUS_VALUES = ["seed", "real_good_data", "real_bad_data", "test_good_data", "test_bad_data"] as const;
 type ClassStatus = typeof CLASS_STATUS_VALUES[number];
 
 export class Class extends Model<InferAttributes<Class>, InferCreationAttributes<Class>> {
@@ -20,82 +20,88 @@ export class Class extends Model<InferAttributes<Class>, InferCreationAttributes
   declare status: CreationOptional<ClassStatus>;
 }
 
-export function initializeClassModel(sequelize: Sequelize) {
-  Class.init({
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    educator_id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-      references: {
-        model: Educator,
-        key: "id"
-      }
-    },
-    created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-    },
-    updated: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: null
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    code: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    asynchronous: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    test: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: 0
-    },
-    seed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    expected_size: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-    },
-    small_class: {
-      type: DataTypes.VIRTUAL,
-      // type: "tinyint(1) GENERATED ALWAYS AS (expected_size < 15) VIRTUAL",
-      get() {
-        return this.expected_size < 15; 
-      }
-    },
-    status: {
-      type: DataTypes.ENUM(...CLASS_STATUS_VALUES),
-      allowNull: true,
-      defaultValue: null,
-    },
-  }, {
+export const CLASS_ATTRIBUTES = {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  educator_id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    references: {
+      model: Educator,
+      key: "id"
+    }
+  },
+  created: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+  },
+  updated: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  asynchronous: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  test: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0
+  },
+  seed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  expected_size: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+  },
+  small_class: {
+    type: DataTypes.VIRTUAL,
+    // type: "tinyint(1) GENERATED ALWAYS AS (expected_size < 15) VIRTUAL",
+    get() {
+      return this.expected_size < 15; 
+    }
+  },
+  status: {
+    type: DataTypes.ENUM(...CLASS_STATUS_VALUES),
+    allowNull: true,
+    defaultValue: null,
+  },
+};
+
+export function classOptions(sequelize: Sequelize) {
+  return {
     sequelize,
     indexes: [
       {
         fields: ["code"],
       }
     ]
-  });
+  };
+}
+
+export function initializeClassModel(sequelize: Sequelize) {
+  Class.init(CLASS_ATTRIBUTES, classOptions(sequelize));
 }
